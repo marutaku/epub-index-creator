e45ca2168f5798a72558be2db5ca095710c8aaf4
Takumi Maruyama <satouredstone@gmail.com>
2024-02-21 22:05:22 +0900

    テスト追加

---
+++ indexer/internal/book/meta_test.go
@@ -0,0 +1,30 @@
+package book
+
+import (
+	"testing"
+)
+
+func TestNewDataFromOPF(t *testing.T) {
+	metadata, err := NewMetaDataFromOPF("testdata/testdata.opf")
+	if err != nil {
+		t.Errorf("Error parsing testdata.xml: %v", err)
+	}
+	if metadata.ISBN != "567812345678" {
+		t.Errorf("ISBN is not correct")
+	}
+	if metadata.Title != "DummyTitle" {
+		t.Errorf("Title is not correct")
+	}
+	if metadata.Language != "en" {
+		t.Errorf("Language is not correct")
+	}
+	if metadata.Author != "DummyAuthor" {
+		t.Errorf("Author is not correct")
+	}
+	if metadata.Publisher != "DummyPublisher" {
+		t.Errorf("Publisher is not correct")
+	}
+	if len(metadata.Pages) != 3 {
+		t.Errorf("Pages is not correct")
+	}
+}
