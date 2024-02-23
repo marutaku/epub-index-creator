4d376e891979b7f6ccaf234fad040926212a855d
Takumi Maruyama <satouredstone@gmail.com>
2024-02-23 23:03:34 +0900

    関数を括り出す

--- indexer/cmd/main.go
+++ indexer/cmd/main.go
@@ -10,27 +10,31 @@ import (
 	"github.com/urfave/cli/v2"
 )
 
+func createIndexFromEPub(epubFilePath string) error {
+	fmt.Printf("epubFilePath: %s\n", epubFilePath)
+	tempDir, err := expand.UnzipEPub(epubFilePath)
+	if err != nil {
+		return err
+	}
+	fmt.Printf("tempDir: %s\n", tempDir)
+	opfFilePath, err := expand.FindOPFFilePath(tempDir)
+	if err != nil {
+		return err
+	}
+	book, err := book.NewBookFromOPF(opfFilePath)
+	if err != nil {
+		return err
+	}
+	fmt.Println(book)
+	return nil
+}
+
 func main() {
 	app := cli.App{
 		Name: "EPub Indexer",
 		Action: func(cCtx *cli.Context) error {
 			epubFilePath := cCtx.Args().First()
-			fmt.Printf("epubFilePath: %s\n", epubFilePath)
-			tempDir, err := expand.UnzipEPub(epubFilePath)
-			if err != nil {
-				return err
-			}
-			fmt.Printf("tempDir: %s\n", tempDir)
-			opfFilePath, err := expand.FindOPFFilePath(tempDir)
-			if err != nil {
-				return err
-			}
-			book, err := book.NewBookFromOPF(opfFilePath)
-			if err != nil {
-				return err
-			}
-			fmt.Println(book)
-			return nil
+			return createIndexFromEPub(epubFilePath)
 		},
 	}
 
