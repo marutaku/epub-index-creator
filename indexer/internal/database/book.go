3b5f6992a02b7018c943bbdefecd07401101a40d
Takumi Maruyama <satouredstone@gmail.com>
2024-02-28 23:17:51 +0900

    永続化層を頑張って作り始める

---
+++ indexer/internal/database/book.go
@@ -0,0 +1,45 @@
+package database
+
+import (
+	"context"
+
+	"github.com/marutaku/epub-index-creator/indexer/ent"
+	"github.com/marutaku/epub-index-creator/indexer/ent/book"
+	"github.com/marutaku/epub-index-creator/indexer/internal/domain"
+)
+
+type BookDatabase struct {
+	client *ent.Client
+}
+
+func NewBookDatabase(sqliteDBPath string) *BookDatabase {
+	client, err := ent.Open("sqlite3", sqliteDBPath)
+	if err != nil {
+		panic(err)
+	}
+	return &BookDatabase{client: client}
+}
+
+func (db *BookDatabase) FindBook(ctx context.Context, isbn string) (*domain.Book, error) {
+	book, err := db.client.Book.Query().
+		Where(book.Isbn(isbn)).
+		Only(ctx)
+	if err != nil {
+		return nil, err
+	}
+	return domain.NewBook(book.Isbn, book.Title, book.Language, book.Author, book.Publisher, nil), nil
+}
+
+func (db *BookDatabase) Save(ctx context.Context, book domain.Book) error {
+	_, err := db.client.Book.Create().
+		SetIsbn(book.ISBN).
+		SetTitle(book.Title).
+		SetLanguage(book.Language).
+		SetAuthor(book.Author).
+		SetPublisher(book.Publisher).
+		Save(ctx)
+	if err != nil {
+		return err
+	}
+	return nil
+}
--- indexer/internal/database/database.go
+++
@@ -1,7 +0,0 @@
-package database
-
-import "github.com/marutaku/epub-index-creator/indexer/internal/domain"
-
-type Database interface {
-	SaveBook(book domain.Book) error
-}
